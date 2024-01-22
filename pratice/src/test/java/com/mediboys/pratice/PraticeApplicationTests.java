bfd6f643abf42d827def4f6451716eb60a7819a5
mediboys <1998smj@naver.com>
2024-01-22 10:48:29 +0900

    init

---
+++ pratice/src/test/java/com/mediboys/pratice/PraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediboys.pratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
